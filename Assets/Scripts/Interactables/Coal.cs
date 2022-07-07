1658b35c4e617443d7ed11bfe7763499a982fec7
JohnArambatzis <61214198+JohnArambatzis@users.noreply.github.com>
2022-07-08 00:58:47 +1000

    Health for Ores, Changed colours for shop buttons
    Added visual health for ores, changed the colour of the shop buttons for buy and sell,

--- Assets/Scripts/Interactables/Coal.cs
+++ Assets/Scripts/Interactables/Coal.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coal : MonoBehaviour
 {
     public GameObject coalCollect;
     public GameObject coal;
+    public GameObject coalCanvas;
 
+    public Text coalHealthText;
     public float timer = 10;
     public float coalHealth = 3;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Bullet")
         {
             coalHealth -= 1;
+            coalHealthText.text = coalHealth.ToString();
+
             if (coalHealth <= 0)
             {
                 gameObject.GetComponent<SpriteRenderer>().enabled = false;
                 gameObject.GetComponent<CircleCollider2D>().enabled = false;
                 gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+                coalCanvas.GetComponent<Canvas>().enabled = false;
 
                 Instantiate(coalCollect, transform.position, Quaternion.identity);
             }
@@ -35,10 +42,12 @@ public class Coal : MonoBehaviour
         {
             coalHealth = 3;
             timer = 10;
+            coalHealthText.text = coalHealth.ToString();
 
             gameObject.GetComponent<SpriteRenderer>().enabled = true;
             gameObject.GetComponent<CircleCollider2D>().enabled = true;
             gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
+            coalCanvas.GetComponent<Canvas>().enabled = true;
         }
     }
 }
--- Assets/Scripts/Interactables/Silver.cs
+++ Assets/Scripts/Interactables/Silver.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Silver : MonoBehaviour
 {
     public GameObject silverCollect;
     public GameObject silver;
+    public GameObject silverCanvas;
 
+    public Text silverHealthText;
     public float timer = 10;
     public float silverHealth = 4;
 
@@ -15,11 +18,14 @@ public class Silver : MonoBehaviour
         if (collision.transform.tag == "Bullet")
         {
             silverHealth -= 1;
+            silverHealthText.text = silverHealth.ToString();
+
             if (silverHealth <= 0)
             {
                 gameObject.GetComponent<SpriteRenderer>().enabled = false;
                 gameObject.GetComponent<CircleCollider2D>().enabled = false;
                 gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+                silverCanvas.GetComponent<Canvas>().enabled = false;
 
                 Instantiate(silverCollect, transform.position, Quaternion.identity);
             }
@@ -35,10 +41,12 @@ public class Silver : MonoBehaviour
         {
             silverHealth = 4;
             timer = 12;
+            silverHealthText.text = silverHealth.ToString();
 
             gameObject.GetComponent<SpriteRenderer>().enabled = true;
             gameObject.GetComponent<CircleCollider2D>().enabled = true;
             gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
+            silverCanvas.GetComponent<Canvas>().enabled = false;
         }
     }
 }
